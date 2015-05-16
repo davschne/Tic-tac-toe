40c9452e34edeca5983f1928da3d0ad6c42a0308
davschne <davschne@gmail.com>
2015-05-15 18:26:11 -0700

    Implemented score-keeping -Created round function -Players alternate taking first move each round -Scoreboard increments according to the outcome of each round

--- tictactoe.js
+++ tictactoe.js
@@ -24,14 +24,16 @@ var board = {
 }
 
 var stats = {
+	// Make a constructor for display item
+	// include value property and display method
 	round: 1,
 	currentPlayer: "X",
-	scoreX: 0,
-	scoreO: 0,
-	ties: 0,
-	display: function(item) {
-		var key = String(item);
-		var value = String(this[item]);
+	X: 0,
+	O: 0,
+	tie: 0,
+	display: function(prop) {
+		var key = String(prop);
+		var value = String(this[prop]);
 		var el = document.getElementById(key);
 		el.textContent = value;
 	},
@@ -51,35 +53,43 @@ function displayMessage(string) {
 
 
 // modify this to take no input but return winner
-function checkWin(player) {
-	var symbol = player.symbol;
+function checkWin() {
 	var grid = board.grid;
+	var findPlayerBySymbol = function(symbol) {
+		if (symbol == playerX.symbol) return playerX;
+		else return playerO;
+	}
+
 	for (var i = 0; i < 3; i++) {
 		//check horizontals
-		if (grid[i][0] === symbol &&
-				grid[i][1] === symbol &&
-				grid[i][2] === symbol) {
-			return true;
+		var symbol = grid[i][0];
+		if (symbol &&
+			  symbol == grid[i][1] &&
+			  symbol == grid[i][2]) {
+			return findPlayerBySymbol(symbol);
 		}
 		//check verticals
-		if (grid[0][i] === symbol &&
-				grid[1][i] === symbol &&
-				grid[2][i] === symbol) {
-			return true;
+		symbol = grid[0][i];
+		if (symbol &&
+			  symbol == grid[1][i] &&
+			  symbol == grid[2][i]) {
+			return findPlayerBySymbol(symbol);
 		}
 	}
 	//check diagonals
-	if (grid[0][0] === symbol &&
-			grid[1][1] === symbol &&
-			grid[2][2] === symbol) {
-		return true;
+	symbol = grid[0][0];
+	if (symbol &&
+		  symbol == grid[1][1] &&
+		  symbol == grid[2][2]) {
+		return findPlayerBySymbol(symbol);
 	}
-	if (grid[0][2] === symbol &&
-			grid[1][1] === symbol &&
-			grid[2][0] === symbol) {
-		return true;
+	symbol = grid[0][2];
+	if (symbol &&
+		  symbol == grid[1][1] &&
+		  symbol == grid[2][0]) {
+		return findPlayerBySymbol(symbol);
 	}
-	return false;
+	return;
 }
 
 function checkTie() {
@@ -92,10 +102,14 @@ function checkTie() {
 }
 
 function turn(player) {
+	// Update and display current player
+	stats.currentPlayer = player.symbol;
+	stats.display("currentPlayer");
 	// Player selects a cell:
-	var choice = Number(prompt(player.symbol + "'s turn. Please select a cell:\n1 2 3\n4 5 6\n7 8 9"));
+	var choice = Number(prompt("Please select a cell:\n1 2 3\n4 5 6\n7 8 9"));
 	while (!choice || choice < 1 || choice > 9) {
-		choice = Number(prompt("Invalid input. Please select a cell:\n1 2 3\n4 5 6\n7 8 9"));
+		displayMessage("Invalid input.");
+		choice = Number(prompt("Please select a cell:\n1 2 3\n4 5 6\n7 8 9"));
 	}
 	var row = Math.floor((choice - 1)/ 3);
 	var col = (choice - 1) % 3;
@@ -112,27 +126,26 @@ function turn(player) {
 	board.display();
 
 	// Check if game is won:
-	if (checkWin(player)) {
+	if (checkWin()) {
 		displayMessage(player.symbol + " wins!");
+		stats[player.symbol]++;
 	} else if (checkTie()) {
 		// Check if game is a tie:
 		displayMessage("The game is a tie.");
+		stats.tie++;
 	} else {
 		// Trigger next turn:
 		turn(player.nextPlayer);
 	}
 }
 
-function round() {
-	// initialize
-	// call turn
-	// return winner or null (if tie)
+function round(startingPlayer) {
+	stats.displayAll();
+	board.init();
+	board.display();
+	turn(startingPlayer);
+	stats.round++;
+	round(startingPlayer.nextPlayer);
 }
 
-// Loop:
-// call round(firstPlayer)
-// update stats based on who won (return value of round)
-
-board.init();
-stats.displayAll();
-turn(playerX);
+round(playerX);
